266865aa7faee4f8a5314f498f06690982571d93
Troels Lenda Strand <you@example.com>
2018-12-18 22:41:04 +0100

    Removing empty scss file

--- src/app/views/intro/intro.component.ts
+++ src/app/views/intro/intro.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
-  templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  templateUrl: './intro.component.html'
 })
 export class IntroComponent {
 
